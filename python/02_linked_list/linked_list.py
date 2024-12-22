bcae09c1dd71d9b622f6a1836d778550ba519748
党康妮 <dangkangni@Admins-MacBook-Air.local>
2024-12-22 12:00:36 +0800

    linked list 241222

--- python/02_linked_list/linked_list.py
+++ python/02_linked_list/linked_list.py
@@ -49,7 +49,7 @@ class LinkedList:
     # 先建new node
     # check if head is none
     # else 挪屁股直到下一个node是none
-    def inserAtEnd(self, data):
+    def insertAtEnd(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -61,4 +61,96 @@ class LinkedList:
 
         current_node.next = new_node
 
+
+    def updateNode(self, val, index):
+        current_node = self.head 
+        position = 0 
+        if position == index:
+            current_node.data = val 
+        else: 
+            while (current_node != None and position != index):
+                position += 1
+                current_node = current_node.next 
+            if current_node != None:
+                current_node.data = val 
+            else:
+                print("Index not present")
+
+
+    def remove_first_node(self):
+        # if nothing to remove 
+        if(self.head == None):
+            return
+        # remove head, replace head with head next 
+        self.head = self.head.next
+
+
+    def remove_last_node(self):
+        # if head is None 
+        if self.head is None:
+            return
+
+        curr_node = self.head
+        while (curr_node.next != None and curr_node.next.next != None):
+            curr_node = curr_node.next
+
+        curr_node.next = None
+
+
+    # Method to remove at given index
+    def remove_at_index(self, index):
+        # 如果链表是空的，则什么都不做
+        if self.head is None:
+            return
+
+        current_node = self.head
+        position = 0 
+        # 如果指定删除的Node节点是链表的头节点
+        if index == 0:
+            self.remove_first_node()
+
+        else:
+            while current_node.next != None: 
+                
+                if position + 1 == index: 
+                    current_node.next = current_node.next.next 
+                    return 
+                else: 
+                    position += 1
+                    current_node = current_node.next 
+                
+            if index > position:
+                print("index not present")
+                return  
+
+    def printLL(self):
+        current_node = self.head
+        while(current_node):
+            print(current_node.data)
+            current_node = current_node.next
+
+
+    def sizeOfLL(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size = size+1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
     
+# create a new linked list
+llist = LinkedList()
+
+# add nodes to the linked list
+llist.insertAtEnd('a')
+llist.insertAtEnd('b')
+llist.insertAtBegin('c')
+llist.insertAtEnd('d')
+llist.insertAtIndex('g', 2)
+
+# print the linked list
+print("Node Data:")
+llist.printLL()
